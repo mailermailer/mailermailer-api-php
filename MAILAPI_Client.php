81cea0434880a00bda2be53b71e5cbfd7d3cda39
mattecalcio <mattecalcio@gmail.com>
2013-08-28 15:12:11 -0400

    Add suppress call and example

--- MAILAPI_Client.php
+++ MAILAPI_Client.php
@@ -78,6 +78,20 @@ class MAILAPI_Client
     }
     
     /**
+     * Suppress the subscriber email address.
+     *
+     * @param string $subscriber_email email of the subscriber to suppress
+     * @return true | MAILAPI_Error
+     */
+    public function suppress($subscriber_email)
+    {
+        $params                       = array();
+        $params['subscriber_email']   = php_xmlrpc_encode($subscriber_email);
+        $response = $this->mailapi_call->executeMethod('suppress', $params);
+        return MAILAPI_Client::getResult($response);
+    }
+
+    /**
      * Formats the response as necessary.
      *
      * @param  mixed $response xmlrpc encoded response from server
