76355cb24752fc8feef2928b5252a30430ecb687
Kevin H. Kamel <kamelkev@underprint.com>
2014-01-10 00:24:33 -0500

    MM-9312
    Update LICENSE to 1.0.5

--- LICENSE.php
+++ LICENSE.php
@@ -21,7 +21,7 @@
  *            along with this program; if not, write to the Free Software
  *            Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
  *            MA 02110-1301, USA.
- * @version   1.0.3
+ * @version   1.0.5
  * @link      http://www.mailermailer.com/api/index.rwp
  */
 
